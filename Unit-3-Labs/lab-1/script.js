705d2d6929e962be66d8901ee075321ee8a36533
Matai Kruk <matai.kruk@gmail.com>
2023-10-16 22:12:36 -0400

    reorganized labs

---
+++ Unit-3-Labs/lab-1/script.js
@@ -0,0 +1,15 @@
+let firstName = "Matai";
+let lastName = "Kruk";
+fullName = firstName + " " + lastName;
+console.log(fullName);
+let population = 634464;
+console.log(population);
+console.log(population*3);
+let javaScriptIsCool = true;
+const colors = ["red", "orange", "yello", "green", "blue", "purple"];
+for(i=1;i<=5;i++) {
+    console.log(i)
+}
+for(i=0;i<=4;i++) {
+    console.log(i)
+}
