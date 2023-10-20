d766807620a20d73d15127ecf9bacbc47f594161
Matai Kruk <matai.kruk@gmail.com>
2023-10-19 22:15:05 -0400

    added unit 3 lab 5

--- Unit-3/lab-3/script.js
+++ Unit-3/lab-3/script.js
@@ -1,10 +1,10 @@
-const name1 = "Lames Baxter"
-const name2 = "Ed"
-const name3 = "James Baxter"
+let name1 = "Lames Baxter"
+let name2 = "Ed"
+let name3 = "James Baxter"
 
-length1 = name1.length
-length2 = name2.length
-length3 = name3.length
+let length1 = name1.length
+let length2 = name2.length
+let length3 = name3.length
 
 switch(true) {
     case length1 > length2 && length1 > length3:
@@ -19,7 +19,7 @@ switch(true) {
         console.log(`${name3} has the longest name.`)
         break;
 
-    case length1 === length2 && length1 == length3:
+    case length1 === length2 && length1 === length3:
         console.log(`All 3 names are the same length.`)
         break;
 
